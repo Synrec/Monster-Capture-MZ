bba2122172fe95324635a8cf7e6e128d91fa656f
Synrec <kylestclr@gmail.com>
2024-03-29 08:30:31 -0400

    Re-implement preload param parser

--- js/plugins/Synrec_Preloader.js
+++ js/plugins/Synrec_Preloader.js
@@ -32,11 +32,13 @@
  * @default 1
  * 
  * @param Ignored Directories
+ * @parent General Settings
  * @desc These directories are ignored by the preloader
  * @type text[]
  * @default []
  * 
  * @param Ignored Files
+ * @parent General Settings
  * @desc These files are ignored by the preloader
  * @type struct<ignoreFile>[]
  * @default []
@@ -90,3 +92,39 @@
 
 const Syn_Preload = {};
 Syn_Preload.Plugin = PluginManager.parameters(`Synrec_Preloader`);
+
+function IGNORE_FILE_PARSER_PRELOAD(obj){
+    try{
+        obj = JSON.parse(obj);
+        return obj;
+    }catch(e){
+        return;
+    }
+}
+
+try{
+    Syn_Preload.IGNORED_FILES = JSON.parse(Syn_Preload.Plugin['Ignored Files']).map((file_data)=>{
+        return IGNORE_FILE_PARSER_PRELOAD(file_data);
+    }).filter(Boolean)
+}catch(e){
+    Syn_Preload.IGNORED_FILES = [];
+}
+
+function PRELOAD_GAUGE_SETTINGS_PARSER_PRELOAD(obj){
+    try{
+        obj = JSON.parse(obj);
+        return obj;
+    }catch(e){
+        const obj = {};
+        obj['Position X'] = 0;
+        obj['Position Y'] = 0;
+        obj['Width'] = Graphics.width;
+        obj['Height'] = Graphics.height;
+        obj['Color'] = '#ffffff';
+        return obj;
+    }
+}
+
+Syn_Preload.PRELOAD_GAUGE_SETTINGS = PRELOAD_GAUGE_SETTINGS_PARSER_PRELOAD(Syn_Preload.Plugin['Loading Gauge']);
+
+Syn_Preload.BYPASS_LOAD_CONFIRM = eval(Syn_Preload.Plugin['Bypass Load Confirm']);
